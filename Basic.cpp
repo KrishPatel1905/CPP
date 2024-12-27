632eb6cc46ca48824065e7c8f3ac3693a6dfa21b
Krish patel <krishhpatel1905@gmail.com>
2024-12-27 12:59:41 +0530

    Basic

---
+++ Basic.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+// cout<<"Hello, Guys";
+// cout<<endl;
+// cout<<"Hello, budy ";
+cout<<"Hello, Guys"<<endl<<"Hello,Buddy";
+
+}
---
+++ Basic/variable.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y,A;
+    x=5,y=5;
+    A=x+y;
+    cout<<"The value of x+y is ::"<<A<<endl;
+    cout<<"The value of x-y is ::"<<x-y<<endl;
+    cout<<"The value of x*y is ::"<<x*y<<endl;
+    cout<<"The value of x/y is ::"<<x/y<<endl;
+}
