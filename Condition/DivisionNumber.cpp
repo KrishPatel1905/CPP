67b53b02fc834041b424d414a9389d04212e3655
Krish patel <krishhpatel1905@gmail.com>
2025-01-07 21:39:28 +0530

    Final Commit

---
+++ Condition/DivisionNumber.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    int num;
+    cout<<"Enter the number:: ";
+    cin>>num;
+    cout<<"Enetr the valuer of n ::";
+    cin>>n;
+    if(num%n==0)
+    {
+        cout<<"The number devied by "<<n;
+    }
+    else{
+        cout<<"The number id not devied by "<<n;
+    }
+    
+
+}
