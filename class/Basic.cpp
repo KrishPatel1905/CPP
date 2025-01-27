f3e93c096ce81d86eff1f2cb82be86141f5a7555
Krish patel <krishhpatel1905@gmail.com>
2025-01-27 17:50:39 +0530

    Intial Commit

--- class/Basic.cpp
+++ class/Basic.cpp
@@ -1,47 +1,37 @@
 #include<iostream>
-#include<string>
 using namespace std;
-
-class Teacher {
-    private:
-        float salary;
-
+class employee
+{
     public:
-        string name;
-        string dep;
-        string subject;
-        void Setsalary (double s)
-        {
-              salary=s;
-        }
-        float Getsalary()
-        {
-            return salary;      
-        }
+    int a, b, c;
+     void setData(int a ,int b, int c);
+    void getData();
+
+       private:
+    int d, e;
+   
 };
+    
 
-// Global declaration of the Teacher object
-Teacher t1;
-
-void Display();
-
-int main() {
-    cout << "Enter Name: ";
-    cin >> t1.name;
-
-    cout << "Enter Department: ";
-    cin >> t1.dep;
-
-    cout << "Enter Subject: ";
-    cin >> t1.subject;
-    t1.Setsalary(80000);
-    Display();
-    return 0;
+ 
+void employee ::setData(int a1, int b1, int c1)
+{
+    a=a1;
+    b=b1;
+    c=c1;
+}
+void employee::getData() {
+    cout << "The value of a: " << a << endl;
+    cout << "The value of b: " << b << endl;
+    cout << "The value of c: " << c << endl;
 }
 
-void Display() {
-    cout << "Name: " << t1.name << endl;
-    cout << "Department Name: " << t1.dep << endl;
-    cout << "Subject Name: " << t1.subject << endl;
-    cout<<  "Salary :"<<t1.Getsalary()<<endl;
+int main()
+{
+    employee emp;
+    emp.setData(1,2,3);
+    emp.getData();
+    
+
+
 }
