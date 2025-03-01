b1b3d7f6210ccfc38f095264fe251cbbddc8effd
Krish patel <krishhpatel1905@gmail.com>
2025-03-01 19:08:39 +0530

    Intial Commit

--- CODE/SingleLineNumberSwap.cpp
+++ CODE/SingleLineNumberSwap.cpp
@@ -5,7 +5,7 @@ int main()
     int a=10; int b=20;
      cout<<"A :"<<a<<endl;
      cout<<"B :"<<b<<endl;
-     a=a+b-(b=a);
+     a=a+b-(b=a);//single line 
      cout<<"Now :\n"<<"A:"<<a<<endl<<"B :"<<b;
     return 0;
 }
