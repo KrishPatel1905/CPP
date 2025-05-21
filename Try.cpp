f94b1dc1a107b35e4df48f0fba03fdbdc79e3c81
Krish patel <krishhpatel1905@gmail.com>
2025-05-21 19:12:02 +0530

    Final Commit

--- Try.cpp
+++ Try.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 int main() {
-    std::string name;
+    string name;
     int age;
 
     // Input user name
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
+    cout << "Enter your name: ";
+    getline(cin, name);
 
     // Input user age
-    std::cout << "Enter your age: ";
-    std::cin >> age;
+    cout << "Enter your age: ";
+    cin >> age;
 
     // Output greeting and years until 100
-    std::cout << "Hello, " << name << "!" << std::endl;
+    cout << "Hello, " << name << "!" << endl;
 
     if (age < 100) {
-        std::cout << "You will turn 100 in " << (100 - age) << " years." << std::endl;
+        cout << "You will turn 100 in " << (100 - age) << " years." << endl;
     } else if (age == 100) {
-        std::cout << "Wow! You're 100 years old today!" << std::endl;
+        cout << "Wow! You're 100 years old today!" << endl;
     } else {
-        std::cout << "You turned 100 " << (age - 100) << " years ago!" << std::endl;
+        cout << "You turned 100 " << (age - 100) << " years ago!" << endl;
     }
 
     return 0;
